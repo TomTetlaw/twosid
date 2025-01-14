5d5b1ee37ef5d3aadc1d11b4e688152ad6eb71bf
Thomas Tetlaw <thomas@tetlaw.id.au>
2025-01-15 03:59:03 +0800

    wip

--- run_tree/shaders/debug.hlsl
+++ run_tree/shaders/debug.hlsl
@@ -37,6 +37,5 @@ Frag_Input vertex_main(Vertex_Input input, uint instance_id: SV_InstanceId) {
 }
 
 float4 fragment_main(Frag_Input input): SV_Target {
-	return float4(1,1,1,1);
 	return input.colour;
 }
